6b00f17df9adff92f342a6f4b21d6b5174ff0b47
CoderAiswarya3004 <aiswaryaayaskant3004@gmail.com>
2026-08-11 08:27:55 +0530

    String and its method , & BOM

---
+++ 04 JavaScript/lec 12-strings/template literal.js
@@ -0,0 +1,9 @@
+const a = 10
+const b = 20
+
+const str = "a value is " + a + " , b value is " + b + " Sum of a & b is " + (a+b)
+console.log(str)
+
+const strtemp =`a value is ${a}, b value is ${b} , Sum of a & b is ${(a+b)}`
+console.log(strtemp)
+
