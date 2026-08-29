a3f286e10c2d66a0c65c1ddf33f7bf155b3aec83
CoderAiswarya3004 <aiswaryaayaskant3004@gmail.com>
2026-08-29 14:40:19 +0530

    async ,await , import ,export ,api

---
+++ 04 JavaScript/lec 17 -The End/import.js
@@ -0,0 +1,30 @@
+// const personName = "Ankit"
+
+//Named Import
+// import {personName} from "./export.js"
+
+
+// console.log(obj);
+
+// //Default Import
+// import personName from "./export.js"
+
+// console.log(personName)
+
+/*
+    1.Named Export
+    2.Default Export
+*/
+
+//====================================
+// 1. Named Import
+// import { username,sayHello,user } from "./export.js"
+
+// console.log('username is',username)
+// sayHello()
+// console.log('user obj',user)
+
+//2.Default Import
+import name from "./export.js"
+console.log(name())
+
